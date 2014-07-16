2fb528d75a5289fbcaed78d8247a36d248b28c33
test248 <test@mail.com>
2014-07-16 07:37:14 +0200

    piccole modifiche!

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string s = "Hello World!";
+    cout << s << endl;
     return 0;
 }
